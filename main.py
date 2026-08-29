12f5aebc7f6df391d80d1119884d5701ed6ff979
Dragonrster <70128194+Dragonrster@users.noreply.github.com>
2026-08-30 03:37:34 +0800

    领取赏钱显示红包总金额

--- main.py
+++ main.py
@@ -1238,14 +1238,15 @@ class MyPlugin(Star):
                 yield event.plain_result("发放失败，请稍后再试。")
                 return
             sname = rp.get("sender_name", rp.get("sender_qq", "?"))
+            total_amt = rp.get("total", 0)
             # 检查是否领完
             if rp["remaining_count"] <= 0:
                 yield event.plain_result(
-                    f"你从 {sname} 的赏钱中领到了 {amount} 铜钱！赏钱已被领完！"
+                    f"你从 {sname} 的赏钱中领到了 {amount} 铜钱！共 {total_amt} 铜钱，赏钱已被领完！"
                 )
             else:
                 yield event.plain_result(
-                    f"你从 {sname} 的赏钱中领到了 {amount} 铜钱！剩余 {rp['remaining_count']} 个"
+                    f"你从 {sname} 的赏钱中领到了 {amount} 铜钱！共 {total_amt} 铜钱，剩余 {rp['remaining_count']} 个"
                 )
         # 检查过期赏钱，退回
         expired = self._expire_red_packets()
